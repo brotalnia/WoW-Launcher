824953316c5a75178700ad32b6cc33e8afea2130
brotalnia <brotalnia@gmail.com>
2017-04-05 22:21:25 +0300

    New vanilla version.
    Adding the updated launcher for Vanilla.

--- Classic Launcher/WoW_Launcher_private.h
+++ Classic Launcher/WoW_Launcher_private.h
@@ -1,17 +1,17 @@
 /* THIS FILE WILL BE OVERWRITTEN BY DEV-C++ */
 /* DO NOT EDIT ! */
 
-#ifndef WOW_LAUNCHER_2_PRIVATE_H
-#define WOW_LAUNCHER_2_PRIVATE_H
+#ifndef WOW_LAUNCHER_PRIVATE_H
+#define WOW_LAUNCHER_PRIVATE_H
 
 /* VERSION DEFINITIONS */
-#define VER_STRING	"1.0.0.0"
-#define VER_MAJOR	1
+#define VER_STRING	"5.0.0.0"
+#define VER_MAJOR	5
 #define VER_MINOR	0
 #define VER_RELEASE	0
 #define VER_BUILD	0
 #define COMPANY_NAME	"brotalnia"
-#define FILE_VERSION	"1.0.0.0"
+#define FILE_VERSION	"5.0.0.0"
 #define FILE_DESCRIPTION	"WoW Launcher for Vanilla"
 #define INTERNAL_NAME	"WoW Launcher"
 #define LEGAL_COPYRIGHT	"Copyright © brotalnia 2015"
@@ -20,4 +20,4 @@
 #define PRODUCT_NAME	"Realmlist Changer and WoW Launcher"
 #define PRODUCT_VERSION	"1.0.0.0"
 
-#endif /*WOW_LAUNCHER_2_PRIVATE_H*/
+#endif /*WOW_LAUNCHER_PRIVATE_H*/
--- Classic Launcher/resource.h
+++ Classic Launcher/resource.h
@@ -1,2 +1,3 @@
 #define IDI_MYICOB  101
 #define IDI_MYICOS  201
+#define IDI_MYICOC  301
